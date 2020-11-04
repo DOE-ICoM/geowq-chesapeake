eedad82faad2f95a356fb79ef3a3235141e3186b
Mgetta <43219719+Mgetta@users.noreply.github.com>
2020-11-04 13:23:46 -0700

    Update extract_pixel_values.py

--- Code/mapping_to_pixel/extract_pixel_values.py
+++ Code/mapping_to_pixel/extract_pixel_values.py
@@ -23,27 +23,10 @@ def formatDate(feat):
      # Get the Yyear-mont-day for each datetime, used to reduce the in-situ data
      dateYMD = datetime.format('yyyy-MM-dd')
      dateNumber = ee.Date.parse('yyy-MM-dd',dateYMD).millis()
-     systemDate =datetime.millis()
+     systemDate = datetime.millis() #incase we use other products in the future
      
      # Save to feature
      return feat.set({'datetime': datetime,'hour': decimalHour,'date':dateYMD,'system:time_start':systemDate, 'dateNumberYMD':dateNumber})
-
-
-def getLonLat(feat):
-    ''' Adds the latitude and longitude as columns to the asset taken from the geometry information.
-    Asssumes the geometry is in the form of a Point().'''
-    
-    feat = feat.set('longitude',feat.geometry().coordinates().get(0))
-    feat = feat.set('latitude',feat.geometry().coordinates().get(1))
-    return feat
-
-
-def setGeometry(feat):  
-    ''' adds geometry information to a feature using the longitude and latitude columns. 
-    Assumes the latitude and longitude are points '''
-    
-    point = ee.Geometry.Point(ee.List([feat.get('longitude'), feat.get('latitude')]))    
-    return ee.Feature(point).copyProperties(feat)
 
 def pixelDateID(feat):
     ''' Create a unique string id based on the modis pixel ID and the YMD date.
@@ -53,45 +36,23 @@ def pixelDateID(feat):
     pixel_id = ee.Algorithms.String(feat.get('pix_id')).cat('_')
     return feat.set('pixelDateID',pixel_id.cat(dateString))
 
-def join_fc_properties(fc1, fc2, field_match='system:index'):
-    """
-    Combines the properties of two FeatureCollections that contain the same
-    features. The features are defined by "system:index" property.
-    """
-    
-    def combine_properties(feature):
-        newfeat = ee.Feature(feature.get('primary')).copyProperties(feature.get('secondary'))
-        return newfeat
-            
-    fc_filter = ee.Filter.equals(
-            leftField = field_match,
-            rightField = field_match)
-       
-    join = ee.Join.inner('primary', 'secondary')
-    do_join = join.apply(fc1, fc2, fc_filter)
-    
-#    clean_join = do_join.map(combine_properties, opt_dropNulls=True)
-    clean_join = do_join.map(combine_properties)
-    
-    return clean_join
-
-
-def getValuesMean(nestedDict,colSelectors,groupName):
-    ''' Convert the nested dictionary outputed by the reduceColumns() function
-    using a mean operator into a feature. Could generalize to other reduction
-    operators though some (such as the count operator) have ouput different 
-    dictionary structures'''
-    def getValues(eeDict):        
-        values = ee.Dictionary(eeDict).values([groupName,'mean']).flatten()
-        newDict = ee.Dictionary.fromLists(colSelectors,values)
-        return ee.Feature(None, newDict)
-    return nestedDict.map(getValues)
-
-
-def getValuesCount(eeDict):
-    ''' Convert the dictionary outputed by the reduceColumns() using the count operator into a feature'''
-    return ee.Feature(None, eeDict)
-
+def reduceFeatCol(featCol,indexCol):  
+    ''' Spatilaly average the data by stepping through each feature date in the
+    date collection, filtering the in-situ data for that date and create a new 
+    feat to return populated by the averages for each of the variables. Takes the
+    geometry/date from the first feature in the in-situ data feature collection '''
+    def mapFeatCol(feat):
+        subset = featCol.filter(ee.Filter.eq('pixelDateID',feat.get('pixelDateID')))
+        newFeat = ee.Feature(subset.first().geometry(),{})
+        newFeat = newFeat.set({'SST (C)' : subset.aggregate_mean('SST (C)'),
+                            'SSS (psu)' : subset.aggregate_mean('SSS (psu)'),
+                            'turbidity (NTU)': subset.aggregate_mean('turbidity (NTU)'),
+                            'count' : subset.aggregate_count('pixelDateID'),
+                            'pix_id' : subset.first().get('pix_id'),
+                            'dateNumberYMD' : subset.first().get('dateNumberYMD'),
+                            'pixelDateID' : feat.get('pixelDateID')})
+        return newFeat
+    return indexCol.map(mapFeatCol)
 
 def reduceFeature(feat):
     ''' Extract modis pixel information fora given lon/lat point. Note that
@@ -102,10 +63,66 @@ def reduceFeature(feat):
     return ee.Feature(feat.setMulti(data))
 
 
+#def setGeometry(feat):  
+#    ''' adds geometry information to a feature using the longitude and latitude columns. 
+#    Assumes the latitude and longitude are points (Only needed when spatially
+#    reducing using the reduceColumns function())'''    
+#    point = ee.Geometry.Point(ee.List([feat.get('longitude'), feat.get('latitude')]))    
+#    return ee.Feature(point).copyProperties(feat)
+#
+#
+#
+#def getLonLat(feat):
+#    ''' Adds the latitude and longitude as columns to the asset taken from the geometry information.
+#    Asssumes the geometry is in the form of a Point(). (Only needed when spatially
+#    reducing using the reduceColumns function())'''
+#    
+#    feat = feat.set('longitude',feat.geometry().coordinates().get(0))
+#    feat = feat.set('latitude',feat.geometry().coordinates().get(1))
+#    return feat
+#
+#def join_fc_properties(fc1, fc2, field_match='system:index'):
+#    """
+#    Combines the properties of two FeatureCollections that contain the same
+#    features. The features are defined by "system:index" property.
+#    """
+#    
+#    def combine_properties(feature):
+#        newfeat = ee.Feature(feature.get('primary')).copyProperties(feature.get('secondary'))
+#        return newfeat
+#            
+#    fc_filter = ee.Filter.equals(
+#            leftField = field_match,
+#            rightField = field_match)
+#       
+#    join = ee.Join.inner('primary', 'secondary')
+#    do_join = join.apply(fc1, fc2, fc_filter)
+#    
+##    clean_join = do_join.map(combine_properties, opt_dropNulls=True)
+#    clean_join = do_join.map(combine_properties)
+#    
+#    return clean_join
+#
+#
+#def getValuesMean(nestedDict,colSelectors,groupName):
+#    ''' Convert the nested dictionary outputed by the reduceColumns() function
+#    using a mean operator into a feature. Could generalize to other reduction
+#    operators though some (such as the count operator) ouput different 
+#    dictionary structures'''
+#    def getValues(eeDict):        
+#        values = ee.Dictionary(eeDict).values([groupName,'mean']).flatten()
+#        newDict = ee.Dictionary.fromLists(colSelectors,values)
+#        return ee.Feature(None, newDict)
+#    return nestedDict.map(getValues)
+#
+#
+#def getValuesCount(eeDict):
+#    ''' Convert the dictionary outputed by the reduceColumns() using the count operator into a feature'''
+#    return ee.Feature(None, eeDict)
 
+    
 
-
-
+    
 ### Actual Code to Run ####
 
 modis_assets = {'daily_500m' : "MODIS/006/MYD09GA",
@@ -127,32 +144,36 @@ params = params = {
 
 
 # Didn't upload the assect with the proper date format so gott reformat and add longitude and latitude as columns
-stations = ee.FeatureCollection('users/jonschwenk/del_chk_insitu_obs').map(formatDate).map(getLonLat)
+stations = ee.FeatureCollection('users/jonschwenk/del_chk_insitu_obs').map(formatDate)
+stations = stations.filter(ee.Filter.lessThan('depth (m)',params['max_depth']))
 
 # reduce in-situ observations temporally
-stations = stations.filter(ee.Filter.lessThan('depth (m)',params['max_depth']))
 hourFilter = ee.Filter.rangeContains('hour', params['window'][0], params['window'][1])
 stations = stations.filter(hourFilter).map(pixelDateID)
-#stations = ee.FeatureCollection(stations.toList(1000, 1000)) # Subset data for testing purposes
+stations = ee.FeatureCollection(stations.toList(1000, 1000)) # Subset data for testing purposes
 
+# reduce in-situ observations spatially (this is where it times-out)
+pixelDateIDs = stations.distinct(['pixelDateID'])
+reducedFeatures = reduceFeatCol(stations,pixelDateIDs)
 
-# Average in-situ observations spatially. NOTE that all columns must be numeric.
-groupName = 'pixelDateID' # column to group observations by when averaging. Can generalize this later
-colSelectors = params['selectors'].copy() # hard-copy pointer reference
-colSelectors.insert(0,groupName)
-colSelectors.extend(['dateNumberYMD','pix_id','longitude','latitude']) # columns to average over
-nCols = len(colSelectors)-1 # number of columns. Needed as ee requires you to repeat the operation each time
-
-# Average over observation values in each unique pixel-YMD combination.  
-average = stations.reduceColumns(selectors = colSelectors,reducer = ee.Reducer.mean().repeat(nCols).group(groupField = 0, groupName = groupName))
-average = ee.FeatureCollection(getValuesMean(average.values(['groups']).flatten(),colSelectors,groupName))
-# Counts requres an arbitrary numeric column 
-counts = stations.reduceColumns(selectors = [groupName,colSelectors[1]],reducer = ee.Reducer.count().group(groupField = 0, groupName = groupName))    
-counts = ee.FeatureCollection(counts.values(['groups']).flatten().map(getValuesCount))
-
-# Combine the feature collections containing the average values and counts 
-reducedFeatures = join_fc_properties(average,counts,groupName).map(setGeometry)
-
+# Original attempt using the reduceColumns() algorithim with the group() method
+## Average in-situ observations spatially. NOTE that all columns must be numeric.
+#groupName = 'pixelDateID' # column to group observations by when averaging. Can generalize this later
+#colSelectors = params['selectors'].copy() # hard-copy pointer reference
+#colSelectors.insert(0,groupName)
+#colSelectors.extend(['dateNumberYMD','pix_id','longitude','latitude']) # columns to average over
+#nCols = len(colSelectors)-1 # number of columns. Needed as ee requires you to repeat the operation each time
+#
+## Average over observation values in each unique pixel-YMD combination.  
+#average = stations.reduceColumns(selectors = colSelectors,reducer = ee.Reducer.mean().repeat(nCols).group(groupField = 0, groupName = groupName))
+#average = ee.FeatureCollection(getValuesMean(average.values(['groups']).flatten(),colSelectors,groupName))
+## Counts requres an arbitrary numeric column 
+#counts = stations.reduceColumns(selectors = [groupName,colSelectors[1]],reducer = ee.Reducer.count().group(groupField = 0, groupName = groupName))    
+#counts = ee.FeatureCollection(counts.values(['groups']).flatten().map(getValuesCount))
+#
+## Combine the feature collections containing the average values and counts 
+#reducedFeatures = join_fc_properties(average,counts,groupName).map(setGeometry)
+#
 
 # Extract pixel information
 IC = ee.ImageCollection(modis_assets[params['asset']])
