d5947af6b7fed653e7ae65379625915ad402d1dd
Mgetta <43219719+Mgetta@users.noreply.github.com>
2020-11-18 10:30:01 -0700

    Updated plotting files
    Added the ability to plot the time series of individual points or map observations on a given day

--- Code/Plotting/plot_data.py
+++ Code/Plotting/plot_data.py
@@ -6,124 +6,69 @@ Created on Tue Nov  3 10:24:41 2020
 """
 
 
+import os
+os.chdir('C:/Users/muklu/Documents/GitHub/satval/Code/Plotting')
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import plot_helpers as ph
 from pyproj import CRS
-
-
-modis_proj4 = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs' # pulled from GEE
-def modisLonLat(p_idx):
-    # Some MODIS grid info for MYDOCGA
-    modis_proj4 = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs' # pulled from GEE
-    modis_gt = (926.625433056, 0, -20015109.354, 0, -926.625433055, 10007554.677) # geotransform pulled from GEE: (xScale, xShearing, xTranslation, yShearing, yScale, yTranslation)
-    modis_shape = (43200,21600) # ncols, nrows
-
-    r, c = np.unravel_index(p_idx, modis_shape[::-1])
-    lons = modis_gt[2] + modis_gt[0] * c + modis_gt[0]/2
-    lats = modis_gt[5] + modis_gt[4] * r + modis_gt[4]/2
-    return lons,lats
-
-
 
 
 # Import the reduced data frame and createa geopandas dataframe. Could skip
 # if importing the geodataframe directly. Assumes that the dataframe has a modis pixel id column
 filepath = 'F:/ICOM/'
-df = pd.read_csv(filepath + 'all_data_reduced.csv')
-df['datetime'] = pd.to_datetime(df['datetime'],format = '%Y-%m-%d %H:%M:%S')
-df.index = df['datetime']
-df['date'] = pd.to_datetime(df['datetime'].dt.strftime('%Y-%m-%d'))
-df.longitude,df.latitude = modisLonLat(df['pix_id'].values)
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude),crs = CRS.from_proj4(modis_proj4))
-gdf = gdf.to_crs(epsg = 4326)
-gdf['longitude'] = gdf.geometry.x
-gdf['latitude'] = gdf.geometry.y 
-
-
-
-# things to adjust
 savepath = 'F:/ICOM/'
+filename = 'unique_pixeldays.csv'
+
+pix_ids = [274463844,270230367] # if you want to plot time series from individual modis pixels
 columns = ['SST (C)','SSS (psu)','turbidity (NTU)']
-startDate = pd.to_datetime('2001-01-01')
-endDate = pd.to_datetime('2021-01-01')
+startDate = '2002-01-01 00:00:00'
+endDate = '2021-01-01 00:00:00'
 extents = [-77.458841,  -74.767094, 36.757802, 39.920274] #polygon bounding box of our aoi (chesapeake_delaware.shp)
 
 
 
-# Number of Modis Pixels containing an insitu observation Each day
-for column in columns:
-    gdfsub = gdf[~pd.isna(df[column])].groupby('date')['pix_id'].nunique().loc[startDate:endDate]   
-    # plot data
-    plt.figure(figsize = (15,10))
-    plt.title('# of Pixels Containing Observations: ' + column,fontsize = 40)
-    plt.plot(gdfsub.index,gdfsub.values,'.k',markersize =6)
-    plt.xlabel('Date',fontsize=32)
-    plt.ylabel('Count',fontsize=32)
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
+# load dataframe will have to tweak things once we get what the actual dataframe will look like
+#gdf = load_dataframe(filepath,filename,datetime_col,sat_pix_col)
+df = pd.read_csv(filepath +filename)
+df = ph.split_pixelday(df) # add 'pix_id' and 'date' columns
+df['datetime'] = pd.to_datetime(df['datetime'],format = '%Y-%m-%d %H:%M:%S')
+df['date'] = pd.to_datetime(df['date'],format = '%Y-%m-%d')
+df.index = df['datetime']
+
+
+### Temporal Plots ###
+# Number of Modis Pixels containing an insitu observation Each day. 
+[ph.plot_counts(df,column) for column in columns]
+
+
+# mean time series of an individual or group of modis pixels
+[ph.plot_timeseries(df,pix_ids,column) for column in columns]
 
 
 
-
-
-# Number of Observations per modis pixel (Note that the count method ignores nan values)
-gdfsub = gdf.groupby(['pix_id','date'])[columns].mean().groupby(level=0).count()
-gdfsub.longitude, gdfsub.latitude = modisLonLat(gdfsub.index.values)
-gdfsub = gpd.GeoDataFrame(gdfsub, geometry=gpd.points_from_xy(gdfsub.longitude, gdfsub.latitude),crs = CRS.from_proj4(modis_proj4))
-gdfsub = gdfsub.to_crs(epsg = 4326)
-gdfsub.longitude = gdfsub.geometry.x
-gdfsub.latitude = gdfsub.geometry.y 
-gdfsub.to_file(savepath + 'counts_spatial') #save as shapefile
-
-# If plotting data as well
+### Spatial Plots ###
 # This uses cartopy for creating maps within python. Alternatively you can make 
 # your own maps in QGIS and I've included a QGIS document with all the relavent 
 # background vector data just add the spatially reduced shapefile saved
 # above 
 
-import cartopy.crs as ccrs
-import cartopy
-from cartopy.feature import NaturalEarthFeature, COLORS
+# Map the number of days containing at least 1 valid observation in each modis pixel
+[ph.map_counts(df,column,startDate = startDate,endDate=endDate,num=100) for column in columns]
 
 
-ocean = NaturalEarthFeature(category='physical', name='ocean',
-                            scale='10m', facecolor=COLORS['water'])
+# map the mean clumn value over a given date range.
+[ph.map_variable(df,column,startDate = startDate,endDate=endDate) for column in columns]
 
-land = NaturalEarthFeature(category='physical', name='land',
-                            scale='10m', facecolor=COLORS['land'])
 
-states_provinces = NaturalEarthFeature(
-        category='cultural',
-        name='admin_1_states_provinces_lines',
-        scale='10m',
-        facecolor='none',
-        edgecolor= 'gray')
 
-# Plot each variable in the reduced dataframe
-for column in columns:
-    plt.figure(figsize=[15,15])
-    
-    # create dummy plot in order to get information for creating the legend
-    ax = plt.scatter(gdfsub.longitude,gdfsub.latitude,s = gdfsub[column],zorder=2)
-    handles, labels = ax.legend_elements(prop="sizes", num=100,  markerfacecolor='none', markeredgecolor='k')
-    
-    # set up the actual background map
-    ax = plt.axes(projection=ccrs.PlateCarree())
-    ax.set_extent(extents)
-    ax.coastlines(resolution='10m', color='black', linewidth=1)
-    ax.add_feature(land)
-    ax.add_feature(ocean)
-    ax.add_feature(states_provinces,linestyle=':')
-    # manuallly add come state labels
-    ax.text(-76.98, 39.56, 'Maryland', transform=ccrs.Geodetic(),color ='gray')
-    ax.text(-76.98, 39.81, 'New York', transform=ccrs.Geodetic(),color ='gray')
-    ax.text(-75.1, 39.56, 'New Jersey', transform=ccrs.Geodetic(),color ='gray')
-    ax.text(-75.5, 38.7, 'Delaware', transform=ccrs.Geodetic(),color ='gray')
-    ax.text(-77.16, 36.93, 'Virginia', transform=ccrs.Geodetic(),color ='gray')
-    
-    # plot the count information and add a rough legend
-    ax.scatter(gdfsub.longitude,gdfsub.latitude,s = gdfsub[column],zorder=2, facecolors='none', edgecolors='k')
-    ax.legend(handles[::25], labels[::25], loc="lower right", title="Number of \n Observations",handletextpad=3.5,labelspacing=1.5,borderpad=3)
-    plt.title('Total Number of Observations: ' + column)
+
+## Save csv to shapefile
+#dfsub = df.loc[startDate:endDate].groupby('pix_id').count()
+#dfsub.longitude, dfsub.latitude = modisLonLat(dfsub.index)
+#gdf = gpd.GeoDataFrame(dfsub, geometry=gpd.points_from_xy(dfsub.longitude, dfsub.latitude),crs = CRS.from_proj4(modis_proj4))
+#gdf = gdf.to_crs(epsg = 4326)
+#gdf.to_file(savepath + 'unique_pixeldays.shp')
+
---
+++ Code/Plotting/plot_helpers.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 15:39:38 2020
+
+@author: muklu
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+from pyproj import CRS
+
+
+modis_proj4 = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs' # pulled from GEE
+
+def modisLonLat(p_idx):
+    # Some MODIS grid info for MYDOCGA
+    modis_proj4 = '+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs' # pulled from GEE
+    modis_gt = (926.625433056, 0, -20015109.354, 0, -926.625433055, 10007554.677) # geotransform pulled from GEE: (xScale, xShearing, xTranslation, yShearing, yScale, yTranslation)
+    modis_shape = (43200,21600) # ncols, nrows
+    r, c = np.unravel_index(p_idx, modis_shape[::-1])
+    lons = modis_gt[2] + modis_gt[0] * c + modis_gt[0]/2
+    lats = modis_gt[5] + modis_gt[4] * r + modis_gt[4]/2
+    return lons,lats
+
+
+
+def split_pixelday(df):
+    ''' taken from the second (unaccepted) answer at https://stackoverflow.com/questions/14745022/how-to-split-a-dataframe-string-column-into-two-columns'''
+    df[['pix_id', 'date']] = df['pixelday'].str.split('_', 1, expand=True)
+    df['pix_id'] = df['pix_id'].astype('int')
+    return df
+
+
+def plot_counts(df,column,startDate = '2001-01-01 00:00:00',endDate = '2021-01-01 00:00:00'):
+    dfsub = df.loc[startDate:endDate].groupby('date').count()
+    plt.figure(figsize = (15,10))
+    plt.title('# of Pixels Containing Observations: ' + column[:-5], fontsize = 40)
+    plt.plot(dfsub.index,dfsub[column],'.k',markersize =6)
+    plt.xlabel('Date',fontsize=32)
+    plt.ylabel('Count',fontsize=32)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+
+# mean time series of an individual or group of modis pixels
+def plot_timeseries(gdf,pix_ids,column,pix_id_col='pix_id',startDate = '2001-01-01 00:00:00',endDate = '2021-01-01 00:00:00'):
+    gdfsub = gdf[gdf[pix_id_col].isin(pix_ids)].groupby([pix_id_col,'date'])[column].mean()
+    fig, ax = plt.subplots(figsize = (15,10))
+    gdfsub.reset_index().pivot('date',pix_id_col,column).plot(ax=ax)
+    # plot time series of each point
+    plt.xlabel('Date',fontsize=32)
+    plt.ylabel(column,fontsize=32)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+
+
+
+
+import cartopy.crs as ccrs
+import cartopy
+from cartopy.feature import NaturalEarthFeature, COLORS
+
+
+ocean = NaturalEarthFeature(category='physical', name='ocean',
+                            scale='10m', facecolor=COLORS['water'])
+
+land = NaturalEarthFeature(category='physical', name='land',
+                            scale='10m', facecolor=COLORS['land'])
+
+states_provinces = NaturalEarthFeature(
+        category='cultural',
+        name='admin_1_states_provinces_lines',
+        scale='10m',
+        facecolor='none',
+        edgecolor= 'gray')
+
+
+
+def map_counts(df,column,startDate='2001-01-01',endDate=None,extents =  [-77.458841,  -74.767094, 36.757802, 39.920274],num=5):
+    
+    if endDate == None:
+        endDate = startDate 
+    
+    dfsub = df.loc[startDate:endDate].groupby('pix_id').aggregate(['mean','count'])
+    
+    if len(dfsub) == 0:
+        return print('No Data on this day')
+    
+
+    plt.figure(figsize=[15,15])
+    
+    
+    # create dummy plot in order to get information for creating the legend
+    ax = plt.scatter(dfsub.longitude['mean'],dfsub.latitude['mean'] ,s = dfsub[column]['count'],zorder=2)
+    handles, labels = ax.legend_elements(prop="sizes", num=num,  markerfacecolor='none', markeredgecolor='k')
+
+    # set up the actual background map
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent(extents)
+    ax.coastlines(resolution='10m', color='black', linewidth=1)
+    ax.add_feature(land)
+    ax.add_feature(ocean)
+    ax.add_feature(states_provinces,linestyle=':')
+    
+    # manuallly add some state labels could probably put in a list for one call
+    ax.text(-76.98, 39.56, 'Maryland', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-76.98, 39.81, 'New York', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-75.1, 39.56, 'New Jersey', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-75.5, 38.7, 'Delaware', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-77.16, 36.93, 'Virginia', transform=ccrs.Geodetic(),color ='gray')
+    
+    # plot the count information and add a rough legend
+    ax.scatter(dfsub.longitude['mean'],dfsub.latitude['mean'],s = dfsub[column]['count'],zorder=2, facecolors='none', edgecolors='k')
+    ax.legend(handles[::int(num/4)], labels[::int(num/4)], loc="lower right", title="Number of \n Observations",handletextpad=3.5,labelspacing=1.5,borderpad=3)
+    plt.title('Total Number of Observations: ' + column)
+    
+    
+    
+def map_variable(df,column,startDate = '2008-04-17',endDate = None,num=5,extents =  [-77.458841,  -74.767094, 36.757802, 39.920274]):
+    
+    if endDate == None:
+        endDate = startDate 
+        
+    dfsub = df.loc[startDate:endDate].groupby('pix_id').mean()
+    if len(dfsub) == 0:
+        return print('No Data on this day')
+    
+    plt.figure(figsize=[15,15])
+     # create dummy plot in order to get information for creating the legend   
+    ax = plt.scatter(dfsub.longitude,dfsub.latitude ,c = dfsub[column],zorder=2)
+    handles, labels = ax.legend_elements(prop="colors", num=num)
+    
+    # set up the actual background map
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent(extents)
+    ax.coastlines(resolution='10m', color='black', linewidth=1)
+    ax.add_feature(land)
+    ax.add_feature(ocean)
+    ax.add_feature(states_provinces,linestyle=':')
+    
+    # manuallly add some state labels could probably put in a list for one call
+    ax.text(-76.98, 39.56, 'Maryland', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-76.98, 39.81, 'New York', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-75.1, 39.56, 'New Jersey', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-75.5, 38.7, 'Delaware', transform=ccrs.Geodetic(),color ='gray')
+    ax.text(-77.16, 36.93, 'Virginia', transform=ccrs.Geodetic(),color ='gray')
+    
+    # plot the count information and add a rough legend
+    ax.scatter(dfsub.longitude,dfsub.latitude,c = dfsub[column],zorder=2)
+    ax.legend(handles, labels, loc="lower right", title=column)
+    plt.title('Average ' + column + ': ' + startDate + ' - ' + endDate)
+
+
+
+
+
+#def load_dataframe(filepath,filename,datetime_col = 'datetime',pix_id_col = 'pix_id',date_format = '%Y-%m-%d %H:%M:%S'):
+#    df = pd.read_csv(filepath + filename)
+#    df['datetime'] = pd.to_datetime(df[datetime_col],format = date_format)
+#    df.index = df['datetime']
+#    df['date'] = pd.to_datetime(df['datetime'].dt.strftime('%Y-%m-%d'))
+#    df.longitude,df.latitude = modisLonLat(df[pix_id_col].values)
+#    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude),crs = CRS.from_proj4(modis_proj4))
+#    gdf = gdf.to_crs(epsg = 4326)
+#    return gdf
