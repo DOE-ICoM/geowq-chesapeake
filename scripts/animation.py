bbc6cf605fc47d963a29c09477fc4ea054d88ded
jsta <stachel2@msu.edu>
2022-08-03 12:39:39 -0600

    add simple plot

--- scripts/animation.py
+++ scripts/animation.py
@@ -19,6 +19,8 @@ def read_xr(date):
     return dt
 
 
+#  --- animation
+
 test = xr.concat([read_xr("2018-01-01"), read_xr("2018-01-02")], dim="time")
 
 variable = test.band_data.sel(time=slice('2018', '2018'))
@@ -26,16 +28,33 @@ variable = test.band_data.sel(time=slice('2018', '2018'))
 fig = plt.figure()
 ax = plt.axes(projection=ccrs.PlateCarree())
 
-image = variable.isel(time = 0).squeeze().plot.imshow(ax=ax, transform=ccrs.PlateCarree(), animated=True)
+image = variable.isel(time=0).squeeze().plot.imshow(
+    ax=ax, transform=ccrs.PlateCarree(), animated=True)
 
-def update(t):    
+
+def update(t):
     # t = variable.time.values[0]
     print(t)
-    ax.set_title("time = %s"%t)
+    ax.set_title("time = %s" % t)
     image.set_array(variable.sel(time=t).squeeze())
     return image,
 
-animation = anim.FuncAnimation(fig, update, frames=variable.time.values, blit=False)
+
+animation = anim.FuncAnimation(fig,
+                               update,
+                               frames=variable.time.values,
+                               blit=False)
 
 #animation.save('tasmax.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+plt.show()
+
+# --- simple plotting
+test = read_xr("2018-01-01")
+da = test.isel(band=0).band_data.expand_dims({"band": 1})
+
+fig = plt.figure()
+ax = plt.axes(projection=ccrs.PlateCarree())
+image = da.squeeze().plot.imshow(ax=ax,
+                                 transform=ccrs.PlateCarree(),
+                                 animated=True)
 plt.show()
