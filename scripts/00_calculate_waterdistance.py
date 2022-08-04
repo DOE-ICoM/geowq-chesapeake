673c2db680f877e961301fcadf7cc8392eaca0ec
jsta <stachel2@msu.edu>
2022-08-04 11:30:52 -0600

    got proof-concept waterdistance calcs working

---
+++ scripts/00_calculate_waterdistance.py
@@ -0,0 +1,54 @@
+# https://gis.stackexchange.com/a/78699/32531
+
+import numpy as np
+import xarray as xr
+import geopandas as gpd
+from skimage.graph import route_through_array
+
+
+def get_idx(lon, lat):
+    # lon = stations.iloc[[0]]["longitude"][0]
+    # lat = stations.iloc[[0]]["latitude"][0]
+    dt_where = dt.sel(x=lon, y=lat, method='nearest')
+    dt_where = dt.where(
+        (dt.x == float(dt_where["x"])) & (dt.y == float(dt_where["y"])),
+        drop=True).squeeze()
+    return (int(dt_where["x_idx"]), int(dt_where["y_idx"]))
+
+
+stations = gpd.GeoDataFrame({
+    "name": ['Choptank', 'Susquehanna', 'Pautexent', 'Potomac'],
+    "longitude": [-75.900, -76.083, -76.698, -77.059],
+    "latitude": [38.811, 39.553, 38.8311, 39.379]
+})
+stations = gpd.GeoDataFrame(stations,
+                            geometry=gpd.points_from_xy(
+                                stations["longitude"], stations["latitude"]))
+
+fpath = "data/cost.tif"
+dt = xr.open_dataset(fpath, engine="rasterio").sel(band=1)
+dt = dt.assign_coords(x_idx=dt["x"] * 0 + [x for x in range(0, 1038)],
+                      y_idx=dt["y"] * 0 + [x for x in range(0, 1448)])
+
+(start_idx_x, start_idx_y) = get_idx(stations.iloc[[0]]["longitude"][0],
+                                     stations.iloc[[0]]["latitude"][0])
+# dt.isel(x=783, y=399)
+(stop_idx_x,
+ stop_idx_y) = get_idx(stations.iloc[[1]].reset_index()["longitude"][0],
+                       stations.iloc[[1]].reset_index()["latitude"][0])
+
+cost_surface_array = dt.to_array().values[0, :, :]
+indices, weight = route_through_array(cost_surface_array,
+                                      (start_idx_y, start_idx_x),
+                                      (stop_idx_y, stop_idx_x),
+                                      geometric=True,
+                                      fully_connected=True)
+indices = np.array(indices).T
+path = np.zeros_like(cost_surface_array)
+path[indices[0], indices[1]] = 1
+
+res = xr.DataArray(path, coords=[dt.y.values, dt.x.values], dims=["y", "x"])
+res["spatial_ref"] = dt["spatial_ref"]
+res = res.where(res > 0)
+
+res.rio.to_raster("data/testasdf2.tif")
--- scripts/00_make_costsurface.py
+++ scripts/00_make_costsurface.py
@@ -2,15 +2,6 @@ import xarray as xr
 import geopandas as gpd
 from geocube.api.core import make_geocube
 
-stations = gpd.GeoDataFrame({
-    "name": ['Choptank', 'Susquehanna', 'Pautexent', 'Potomac'],
-    "longitude": [-75.900, -76.083, -76.698, -77.059],
-    "latitude": [38.811, 39.553, 38.8311, 39.379]
-})
-stations = gpd.GeoDataFrame(stations,
-                            geometry=gpd.points_from_xy(
-                                stations["longitude"], stations["latitude"]))
-
 chk = gpd.read_file("data/Boundaries/chk_water_only.shp").to_crs(4326)
 chk_hull = gpd.GeoDataFrame(geometry=chk.geometry.convex_hull)
 chk_hull["cost"] = 1000
