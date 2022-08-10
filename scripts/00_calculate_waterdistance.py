b43dce24efbdbcf95262038d43228a7aa523e3bf
jsta <stachel2@msu.edu>
2022-08-10 15:44:03 -0600

    resolve mystery coredump error

--- scripts/00_calculate_waterdistance.py
+++ scripts/00_calculate_waterdistance.py
@@ -20,6 +20,8 @@ def get_idx_coords(lon, lat):
     return (int(dt_where["x_idx"]), int(dt_where["y_idx"]))
 
 def get_geo_coords(x, y):
+    # y = 1199
+    # x = 310
     dt_where = dt.where((dt.x_idx==x) & (dt.y_idx==y), drop=True)
     return (float(dt_where["x"]), float(dt_where["y"]))
 
@@ -35,7 +37,7 @@ def get_distance(stop_idx_y, stop_idx_x, i):
     try:
         indices, weight = route_through_array(cost_surface_array,
                                             (start_idx_y, start_idx_x),
-                                            (stop_idx_y, stop_idx_x),
+                                            (stop_idx_x, stop_idx_y),
                                             geometric=True,
                                             fully_connected=True)
         indices = np.array(indices).T
@@ -56,7 +58,8 @@ def get_distance(stop_idx_y, stop_idx_x, i):
         gdf = gpd.GeoDataFrame({"cost": df.cost, "i":i}, geometry=gpd.points_from_xy(df.x, df.y))
 
     except:
-        gdf = gpd.GeoDataFrame({"cost": 999, "i":i}, index=[0])
+        (lon, lat) = get_geo_coords(stop_idx_x, stop_idx_y)
+        gdf = gpd.GeoDataFrame({"cost": 999, "i":i}, geometry=gpd.points_from_xy(lon, lat))            
 
     return gdf
     
@@ -66,9 +69,9 @@ def get_distance_grp(rng):
     last_rng = rng[-1]
     outpath = "test_" + str(last_rng) + ".gpkg"
     if not os.path.exists(outpath):
-        par_njobs = 10  # , verbose=25
-        gdfs = Parallel(n_jobs=par_njobs)(delayed(get_distance)(end_points[i][0],
-                                                    end_points[i][1], i)
+        par_njobs = 12
+        gdfs = Parallel(n_jobs=par_njobs, verbose=25)(delayed(get_distance)(end_points[i][1],
+                                                    end_points[i][0], i)
                             for i in rng)  # end_points.shape[0]
 
         pd.concat(gdfs).to_file(outpath, driver="GPKG")
@@ -89,29 +92,26 @@ dt = xr.open_dataset(fpath, engine="rasterio").sel(band=1)
 dt = dt.assign_coords(x_idx=dt["x"] * 0 + [x for x in range(0, 1038)],
                       y_idx=dt["y"] * 0 + [x for x in range(0, 1448)])
 cost_surface_array = dt.to_array().values[0, :, :]
-
-# for start_pnt i
-(start_idx_x, start_idx_y) = get_idx_coords(stations.iloc[[0]]["longitude"][0],
-                                            stations.iloc[[0]]["latitude"][0])
 end_points = get_idx_water()
 
-# ----
-
-
+# choptank
+(start_idx_x, start_idx_y) = get_idx_coords(stations.iloc[[0]]["longitude"][0],
+                                            stations.iloc[[0]]["latitude"][0])
 end_points.shape[0]  # 294049
 l = np.array_split(np.array(range(0, end_points.shape[0])), 6)
+l[-1] = np.delete(l[-1], np.array([11311])) # resolve mystery coredump error
 [s[-1] for s in l]
-# [get_distance_grp(s) for s in l]
+[get_distance_grp(s) for s in l]
 
-res = []
-for i in tqdm(l[-1]):
-    # print(i)
-    res.append(get_distance(end_points[i][0], end_points[i][1], i))
+# res = []
+# for i in tqdm(l[-1]):
+#     # print(i)
+#     res.append(get_distance(end_points[i][1], end_points[i][0], i))
 
-# i = 11311
-# get_distance(end_points[i][0], end_points[i][1], i)
-# (end_points[11311][0], end_points[11311][1])
-# get_geo_coords(end_points[11311][0], end_points[11311][1])
+# i = l[-1][11311]
+# get_distance(end_points[i][1], end_points[i][0], i)
+# (end_points[i][1], end_points[i][0])
+# get_geo_coords(end_points[i][1], end_points[i][0])
 
 # sum each layer
 # res.rio.to_raster("data/testasdf35222.tif")
