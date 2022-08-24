b5a587024b8ad43cb43c7dead5397896df1dd3fb
jsta <stachel2@msu.edu>
2022-08-24 08:44:24 -0400

    cleanup discharge plot

--- figures/discharge.py
+++ figures/discharge.py
@@ -3,22 +3,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-df3 = pd.read_csv("data/discharge_raw.csv")
-df3["measurement_dt"] = pd.to_datetime(df3["measurement_dt"])
+df = pd.read_csv("data/discharge_raw.csv", parse_dates=True, index_col="measurement_dt")
+df = df[["discharge_va", "site_str"]]
+df.sort_index(inplace=True)
+df = df.groupby("site_str").rolling("365D").median().reset_index()
+df = df[df["measurement_dt"] > pd.to_datetime("1945-01-01 00:00:00")]
 
 fig, ax = plt.subplots()
 
-g = sns.lineplot(data=df3,
-                 x="measurement_dt",
-                 y="discharge_va",
-                 hue="site_str", 
-                 ax=ax)
+g = sns.lineplot(data=df, x="measurement_dt", y="discharge_va", hue="site_str", ax=ax)
 
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles=handles[0:], labels=labels[0:])
+# g.legend(handles,labels, ncol=3)
+sns.move_legend(ax, "upper center", bbox_to_anchor=(0.5, 1.15), ncol=3)
 
 g.set_yscale("log")
-
 plt.xlabel("")
 plt.ylabel("Discharge (cfs)")
 
