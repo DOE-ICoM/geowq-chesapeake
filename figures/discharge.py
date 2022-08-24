3b92a83340a87808c07ad1d8dd113fe7b11d9232
jsta <stachel2@msu.edu>
2022-08-24 10:11:41 -0400

    cleanup

--- figures/discharge.py
+++ figures/discharge.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("data/discharge_raw.csv", parse_dates=True, index_col="measurement_dt")
 df = df[["discharge_va", "site_str"]]
+df = df[df.index > pd.to_datetime("1945-01-01 00:00:00")]
 df.sort_index(inplace=True)
 df = df.groupby("site_str").rolling("365D").median().reset_index()
-df = df[df["measurement_dt"] > pd.to_datetime("1945-01-01 00:00:00")]
 
 fig, ax = plt.subplots()
 
-g = sns.lineplot(data=df, x="measurement_dt", y="discharge_va", hue="site_str", ax=ax)
+g = sns.lineplot(
+    data=df, x="measurement_dt", y="discharge_va", hue="site_str", ax=ax, markers=True
+)
 
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles=handles[0:], labels=labels[0:])
