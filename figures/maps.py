00adbf3e1311e11c483efe62e61db46f2d9f350a
jsta <stachel2@msu.edu>
2022-09-06 16:12:50 -0600

    formatting with yapf

--- figures/maps.py
+++ figures/maps.py
@@ -13,33 +13,29 @@ import h3pandas  # h3.geo_to_h3_aggregate
 sys.path.append(".")
 from src import utils
 
+
 # --- loc_id frequency count histograms and hex map
 def get_pnt_counts(dt, variable):
     # variable = variables[0]
     dt_sub = utils.select_var(dt, variable)
     dt_sub = dt_sub[~pd.isna(dt_sub["value"])]
 
-    res = (
-        dt_sub[dt_sub["variable"] == variable]
-        .groupby(["loc_id", "latitude", "longitude"])
-        .size()
-        .reset_index()
-        .rename(columns={0: "count"})
-        .sort_values("count", ascending=False)
-        .reset_index(drop=True)
-    )
+    res = (dt_sub[dt_sub["variable"] == variable].groupby(
+        ["loc_id", "latitude",
+         "longitude"]).size().reset_index().rename(columns={
+             0: "count"
+         }).sort_values("count", ascending=False).reset_index(drop=True))
 
-    res_gdf = gpd.GeoDataFrame(
-        res, geometry=gpd.points_from_xy(res["longitude"], res["latitude"])
-    )
+    res_gdf = gpd.GeoDataFrame(res,
+                               geometry=gpd.points_from_xy(
+                                   res["longitude"], res["latitude"]))
 
     return res_gdf
 
 
 dt_raw = pd.read_csv(
-    os.environ["ICOM_DATA"]
-    + "/Modeling Data/Processed Data p1/aggregated_w_bandvals.csv"
-)
+    os.environ["ICOM_DATA"] +
+    "/Modeling Data/Processed Data p1/aggregated_w_bandvals.csv")
 id_vars = ["loc_id", "latitude", "longitude"]
 variables = ["SSS (psu)", "turbidity (NTU)", "SST (C)"]
 dt_filtered = dt_raw[id_vars + variables]
@@ -83,7 +79,8 @@ cbar.ax.set_title("obs count (n)", y=-0.08)
 plt.savefig("figures/_freqcount_hex.pdf")
 
 # --- input/output map
-bay_gdf_hires = gpd.read_file("data/Boundaries/chk_water_only.shp").to_crs(epsg=4326)
+bay_gdf_hires = gpd.read_file("data/Boundaries/chk_water_only.shp").to_crs(
+    epsg=4326)
 
 # get cbofs image
 tod = "20220904"
@@ -98,7 +95,8 @@ img_cbofs = img_cbofs["band_data"].sel(band=1)
 # gdalwarp -te -77.3425000000000011 36.1675000000000040 -74.7974999999999994 39.6325000000000074 -ts 509 693 -overwrite 2022-09-04.tif 2022-09-04_downsample.tif
 
 date = "2022-09-04"
-img_rf = xr.open_dataset("data/prediction/" + date + "_downsample.tif", engine="rasterio")
+img_rf = xr.open_dataset("data/prediction/" + date + "_downsample.tif",
+                         engine="rasterio")
 img_rf = img_rf.rio.clip(bay_gdf_hires.geometry)
 img_rf = img_rf["band_data"].sel(band=1)
 img_rf.rio.to_raster("data/prediction/" + date + "_downsample_clip.tif")
@@ -118,24 +116,42 @@ img_gee = img_gee["band_data"].sel(band=1)
 # create raster diff
 lons = img_rf.sortby("y", "x").x.as_numpy().values
 lats = img_rf.sortby("y", "x").y.as_numpy().values
-data = img_rf.sortby("y", "x").to_numpy() - img_cbofs.sortby("y", "x").to_numpy()
-test = xr.DataArray(data=data, dims=["y", "x"], coords=dict(x=(lons), y=(lats)))
+data = img_rf.sortby("y", "x").to_numpy() - img_cbofs.sortby("y",
+                                                             "x").to_numpy()
+test = xr.DataArray(data=data,
+                    dims=["y", "x"],
+                    coords=dict(x=(lons), y=(lats)))
 test = test.rio.set_spatial_dims("x", "y")
 
-def panel_add(i, axs, title, geo_grid, diff=False):    
-    ax = axs[i]        
+
+def panel_add(i, axs, title, geo_grid, diff=False):
+    ax = axs[i]
     if diff:
-        geo_grid.plot.imshow(ax=ax, center=0, cbar_kwargs={"shrink":0.5, 'label':''})
+        geo_grid.plot.imshow(ax=ax,
+                             center=0,
+                             cbar_kwargs={
+                                 "shrink": 0.5,
+                                 'label': ''
+                             })
     else:
-        geo_grid.plot.imshow(ax=ax, vmin=0, cbar_kwargs={"shrink":0.5, 'label':''})# , vmax=np.nanmax(img_cbofs.to_numpy()))
-    ax.set_title(title)    
-    ax.coastlines(resolution="10m", color="black", linewidth=1)    
+        geo_grid.plot.imshow(ax=ax,
+                             vmin=0,
+                             cbar_kwargs={
+                                 "shrink": 0.5,
+                                 'label': ''
+                             })  # , vmax=np.nanmax(img_cbofs.to_numpy()))
+    ax.set_title(title)
+    ax.coastlines(resolution="10m", color="black", linewidth=1)
+
 
 fig, axs = plt.subplots(
     ncols=3,
     nrows=1,
     constrained_layout=True,
-    subplot_kw={"projection": ccrs.PlateCarree(), "xlabel":""},
+    subplot_kw={
+        "projection": ccrs.PlateCarree(),
+        "xlabel": ""
+    },
 )
 
 # st = fig.suptitle("Number of days for each month meeting the criteria in 2017", fontsize="large")
