312bd6c8d4461ab058ce78d239d51634f5ce21e3
jsta <stachel2@msu.edu>
2022-09-13 11:22:13 -0600

    cleanup best_params table

--- figures/rf_stats_table.py
+++ figures/rf_stats_table.py
@@ -6,13 +6,16 @@ sys.path.append(".")
 from src import utils
 
 dt = pd.read_csv("data/best_params_rf.csv")
+dt = dt.drop(columns=["bootstrap", "min_samples_split"])
+variables = dt.pop("variable")
+dt.insert(0, "variable", variables)
 utils.tabulate_to_latex(
     tabulate(
         dt.values, headers=[x for x in dt.columns], tablefmt="latex"
     ),
     "figures/rf_stats_table_0_.tex",
     "Random Forest tuning results.",
-    2,
+    1,
 )
 
 dt = pd.read_csv("data/rmse_rf.csv").round(2)
