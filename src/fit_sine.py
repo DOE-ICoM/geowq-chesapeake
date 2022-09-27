04388b175204d5f937ae66bd776f107a094c1693
jsta <stachel2@msu.edu>
2022-09-27 15:34:32 -0600

    formatting with yapf

--- src/fit_sine.py
+++ src/fit_sine.py
@@ -8,9 +8,8 @@ import matplotlib.pyplot as plt
 
 fitfunc = lambda p, x: p[0] * np.sin(2 * np.pi / p[1] * x + p[2]) + p[3]
 
+
 def fit_sine(data):
-    # data = pd.read_csv('temperature.csv')
-    # data.replace([np.inf, -np.inf], np.nan, inplace=True)
     # data = pd.read_csv("data/aggregated_w_bandvals.csv")
 
     data['datetime'] = pd.to_datetime(data['datetime'])
@@ -24,17 +23,17 @@ def fit_sine(data):
         temp_mean = np.mean(data['SST (C)'][data['datetime'] == i])
         temp_doy.append(temp_mean)
     date_doy = np.arange(1, 367)
-    
+
     errfunc = lambda p, x, y: fitfunc(p, x
                                       ) - y  # Distance to the target function
     p0 = [12.23414294, 368.06372131, 29.41529669, 16.38386361]
-    p1, success = optimize.leastsq(errfunc, p0[:], args=(date_doy, temp_doy))    
+    p1, success = optimize.leastsq(errfunc, p0[:], args=(date_doy, temp_doy))
     # print(success)
-    # print(p1)    
+    # print(p1)
     pickle.dump(p1, open("data/temperature_sine_coef.pkl", "wb"))
 
     fitted_sine = fitfunc(p1, date)
-    
+
     plt.figure(figsize=(20, 10))
     plt.plot(date_doy, temp_doy, "ro")
     plt.plot(date_doy, fitfunc(p1, date_doy), "bo")
