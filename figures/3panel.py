ea51dfa4fa6eb32619525df849555d0983f27b72
Jemma Stachelek <jemma.stachelek@gmail.com>
2024-07-24 13:06:44 -0600

    stash

--- figures/3panel.py
+++ figures/3panel.py
@@ -20,6 +20,7 @@ def panel_add(
     lon_ticks=[],
     lat_ticks=[],
     vector=False,
+    bounds=None,
 ):
     if j is not None:
         ax = plt.subplot(axs[i, j], xlabel="", projection=ccrs.PlateCarree())
@@ -46,10 +47,15 @@ def panel_add(
                 ax=ax,
                 add_labels=False,
                 add_colorbar=False,
-            )  # , vmax=np.nanmax(img_cbofs.to_numpy()))
+            )
     if vector:
         geo_grid = geo_grid.to_crs(ccrs.PlateCarree())
-        geo_grid.plot(column="predict", ax=ax, markersize=0.5, legend=False)
+        geo_grid.sample(4000).plot(column="predict", ax=ax, markersize=0.1, legend=False)
+
+    if bounds is not None:
+        # minx, miny, maxx, maxy
+        ax.set_xlim(bounds[0], bounds[2])
+        ax.set_ylim(bounds[1], bounds[3])
 
     ax.set_title(title, size="small", y=height_frac, x=0.9, rotation="vertical")
 
@@ -70,7 +76,9 @@ img_rf = utils.get_rf_prediction("2022-09-04", "salinity")
 
 pnts_raw = gpd.read_file("test3.gpkg")
 
-# breakpoint()
+# minx, miny, maxx, maxy
+# bounds = pnts_raw.to_crs(ccrs.PlateCarree()).total_bounds
+bounds = (-76.44, 36.88, -75.661, 37.952)
 
 plt.close()
 fig, axs = plt.subplots(
@@ -85,30 +93,37 @@ panel_add(
     "Diffuse missing",
     pnts_raw,
     height_frac=0.65,
-    lat_ticks=[37, 38, 39],
-    lon_ticks=[-77, -76, -75],
+    lat_ticks=[37, 37.5],
+    lon_ticks=[-76.5, -76, -75.5],
     vector=True,
+    bounds=bounds,
 )
 
 
-panel_add(1, axs, "NN-interpolation", img_rf, height_frac=0.6, lat_ticks=[37, 38, 39],
-    lon_ticks=[-77, -76, -75],)
-axs[1].tick_params(colors='white')
+panel_add(
+    1,
+    axs,
+    "NN-interpolation",
+    img_rf,
+    height_frac=0.6,
+    lat_ticks=[37, 38, 39],
+    lon_ticks=[-77, -76, -75],
+    bounds=bounds
+)
+axs[1].tick_params(colors="white")
 
-panel_add(2, axs, "Target", img_cbofs, height_frac=0.8, lat_ticks=[37, 38, 39],
-    lon_ticks=[-77, -76, -75],)
-axs[2].tick_params(colors='white')
+panel_add(
+    2,
+    axs,
+    "Target",
+    img_cbofs,
+    height_frac=0.8,
+    lat_ticks=[37, 38, 39],
+    lon_ticks=[-77, -76, -75],
+    bounds=bounds
+)
+axs[2].tick_params(colors="white")
 
-# fig.delaxes(fig.axes[0])
-# fig.delaxes(fig.axes[1])
-# fig.delaxes(fig.axes[2])
-# plt.tight_layout()
-# axs[0].legend().set_visible(False)
-# axs[1].legend().set_visible(False)
-# plt.legend('',frameon=False)
-# fig.legends = []
-# axs[1].legend_ = None
-# plt.margins(y=1)
 fig.subplots_adjust(top=0.5, bottom=0.1)
 
 # plt.show()
