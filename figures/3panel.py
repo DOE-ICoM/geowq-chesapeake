d3d8b8cee9f41ebae4c2bf0e712759ab3657bf44
Jemma Stachelek <jemma.stachelek@gmail.com>
2024-07-29 09:00:23 -0600

    towards insets

--- figures/3panel.py
+++ figures/3panel.py
@@ -22,7 +22,8 @@ def panel_add(
     lat_ticks=[],
     vector=False,
     bounds=None,
-    missing_blocks=False
+    missing_blocks=False,
+    title_on=True,
 ):
     if j is not None:
         ax = plt.subplot(axs[i, j], xlabel="", projection=ccrs.PlateCarree())
@@ -46,6 +47,8 @@ def panel_add(
             geo_grid.plot.imshow(ax=ax, center=0)
         else:
             geo_grid.plot.imshow(
+                vmin=0,
+                vmax=27,
                 ax=ax,
                 add_labels=False,
                 add_colorbar=False,
@@ -53,30 +56,52 @@ def panel_add(
         # breakpoint()
         # bounds
         if missing_blocks:
+            # rect = patches.Rectangle(
+            #     (-76.196, 37.645), 0.1, 0.1, linewidth=1, edgecolor="gray", facecolor="white"
+            # )
+            # ax.add_patch(rect)
             rect = patches.Rectangle(
-                (-76.196, 37.645), 0.1, 0.1, linewidth=1, edgecolor="gray", facecolor="white"
-            )
-            ax.add_patch(rect)
-            rect = patches.Rectangle(
-                (-76.124, 37.307), 0.1, 0.1, linewidth=1, edgecolor="gray", facecolor="white"
+                (-76.124, 37.307),
+                0.1,
+                0.1,
+                linewidth=1,
+                edgecolor="gray",
+                facecolor="white",
             )
             ax.add_patch(rect)
 
     if vector:
         geo_grid = geo_grid.to_crs(ccrs.PlateCarree())
         geo_grid.sample(5000).plot(
-            column="predict", ax=ax, markersize=0.1, legend=False
+            column="predict",
+            ax=ax,
+            markersize=0.1,
+            legend=False,
+            vmin=-10,
+            vmax=27,
         )
+        # rect = patches.Rectangle(
+        #     (-76.124, 37.307),
+        #     0.1,
+        #     0.1,
+        #     linewidth=1,
+        #     edgecolor="gray",
+        #     facecolor=None,
+        #     linestyle="--",
+        #     alpha=0.5
+        # )
+        # ax.add_patch(rect)
 
     if bounds is not None:
         # minx, miny, maxx, maxy
         ax.set_xlim(bounds[0], bounds[2])
         ax.set_ylim(bounds[1], bounds[3])
 
-    ax.set_title(title, size="small", y=height_frac, x=0.9, rotation="vertical")
+    if title_on:
+        ax.set_title(title, size="small", y=height_frac, x=0.87, rotation="vertical")
 
 
-# --- rf vs cbofs map
+# --- load data
 bay_gdf_hires = gpd.read_file("data/Boundaries/chk_water_only.shp").to_crs(epsg=4326)
 
 # get cbofs image
@@ -96,6 +121,7 @@ pnts_raw = gpd.read_file("test3.gpkg")
 # bounds = pnts_raw.to_crs(ccrs.PlateCarree()).total_bounds
 bounds = (-76.44, 36.88, -75.661, 37.952)
 
+# --- rf vs cbofs map
 plt.close()
 fig, axs = plt.subplots(
     ncols=3,
@@ -116,19 +142,6 @@ panel_add(
 )
 axs[0].tick_params(colors="white")
 
-
-# panel_add(
-#     1,
-#     axs,
-#     "NN-interpolation",
-#     img_rf,
-#     height_frac=0.6,
-#     lat_ticks=[37, 38, 39],
-#     lon_ticks=[-77, -76, -75],
-#     bounds=bounds,
-# )
-# axs[1].tick_params(colors="white")
-
 panel_add(
     1,
     axs,
@@ -138,7 +151,7 @@ panel_add(
     lat_ticks=[37, 38, 39],
     lon_ticks=[-77, -76, -75],
     bounds=bounds,
-    missing_blocks=True
+    missing_blocks=True,
 )
 axs[1].tick_params(colors="white")
 
@@ -159,3 +172,62 @@ fig.subplots_adjust(top=0.5, bottom=0.1)
 # plt.show()
 plt.savefig("test.pdf")
 # plt.savefig("figures/_rf-vs-cbofs.pdf")
+
+# --- insets
+
+
+# minx, miny, maxx, maxy
+# trying adding 0.1
+bounds = (-76.124, 37.307, -76.024, 37.407)
+
+plt.close()
+fig, axs = plt.subplots(
+    ncols=3,
+    nrows=1,
+    # constrained_layout=True,
+    subplot_kw={"projection": ccrs.PlateCarree(), "xlabel": ""},
+)
+
+panel_add(
+    0,
+    axs,
+    "NN-interpolation",
+    img_rf,
+    height_frac=0.6,
+    lat_ticks=[37, 38, 39],
+    lon_ticks=[-77, -76, -75],
+    bounds=bounds,
+    title_on=False,
+)
+axs[0].tick_params(colors="white")
+
+# TODO: block NN fill for the middle panel
+panel_add(
+    1,
+    axs,
+    "NN-interpolation",
+    img_rf,
+    height_frac=0.6,
+    lat_ticks=[37, 38, 39],
+    lon_ticks=[-77, -76, -75],
+    bounds=bounds,
+    title_on=False,
+)
+axs[1].tick_params(colors="white")
+
+panel_add(
+    2,
+    axs,
+    "Target",
+    img_cbofs,
+    height_frac=0,
+    lat_ticks=[37, 38, 39],
+    lon_ticks=[-77, -76, -75],
+    bounds=bounds,
+    title_on=False,
+)
+axs[2].tick_params(colors="white")
+
+fig.subplots_adjust(top=0.5, bottom=0.1)
+
+plt.savefig("test_insets.pdf")
